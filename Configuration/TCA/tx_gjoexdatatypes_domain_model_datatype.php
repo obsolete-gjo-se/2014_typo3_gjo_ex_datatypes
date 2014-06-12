517074356fc8ca21b900df9884eefdcbd35ce0c8
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-12 08:44:49 +0200

    [TASK] - datatype:stringWithoutBreak - Backend

--- Configuration/TCA/tx_gjoexdatatypes_domain_model_datatype.php
+++ Configuration/TCA/tx_gjoexdatatypes_domain_model_datatype.php
@@ -5,19 +5,29 @@ return array(
     'ctrl' => array(
         'title' => 'Table: tx_gjoexdatatypes_domain_model_datatype',
         'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('gjo_ex_datatypes') . 'Resources/Public/Icons/tx_gjoexdatatypes_domain_model_datatype.gif',
-        'label' => '',
+        'label' => 'string_without_break',
 
     ),
     'columns' => array(
+        'string_without_break' => array(
+            'label' => 'Label: stringWithoutBreak',
+            'config' => array(
+                'type' => 'input',
+                'eval' => 'required',
+                'max' => '30',
+            ),
+        ),
     ),
 
     'types' => array(
         '1' => array(
-            'showitem' => ''
-        ),
+            'showitem' => '
+                string_without_break,
+                '),
     ),
 
     'interface' => array(
-        'showRecordFieldList' => '',
-    ),
+        'showRecordFieldList' => '
+            string_without_break,
+            ',),
 );
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,6 +1,8 @@
 CREATE TABLE tx_gjoexdatatypes_domain_model_datatype (
   uid int(11) NOT NULL auto_increment,
   pid int(11) NOT NULL DEFAULT '0',
+  string_without_break varchar(30) NOT NULL DEFAULT '',
+
 
   PRIMARY KEY (uid),
   KEY parent (pid),
