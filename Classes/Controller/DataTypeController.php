80cadc88084aa9a0d1cdfc7db886f60116d7cb4e
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-12 13:18:14 +0200

    [TASK] - datatype:stringWithoutBreak - Frontend, findAll

--- Classes/Controller/DataTypeController.php
+++ Classes/Controller/DataTypeController.php
@@ -5,4 +5,16 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class DataTypeController extends ActionController
 {
+    /**
+     * @var \Gjo\GjoExDatatypes\Domain\Repository\DataTypeRepository
+     * @inject
+     */
+    protected $dataTypeRepository;
+
+    /**
+     * @return void
+     */
+    public function findAllAction() {
+        $this->view->assign('dataTypes', $this->dataTypeRepository->findAll());
+    }
 }
