23f508e4ec48aafc4a6cd27254882c8bac21bab3
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-13 13:44:49 +0200

    [TASK] - datatype:selectSingleDb - Backend

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,6 +7,17 @@ CREATE TABLE tx_gjoexdatatypes_domain_model_datatype (
   password varchar(20) NOT NULL DEFAULT '',
   number_integer int(11) NOT NULL DEFAULT '0',
   select_single int(11) NOT NULL DEFAULT '0',
+  select_single_db int(11) NOT NULL DEFAULT '0',
+
+  PRIMARY KEY (uid),
+  KEY parent (pid),
+);
+
+CREATE TABLE tx_gjoexdatatypes_domain_model_month (
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) NOT NULL DEFAULT '0',
+
+  month_string varchar(30) NOT NULL DEFAULT '',
 
   PRIMARY KEY (uid),
   KEY parent (pid),
