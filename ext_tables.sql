c477b281c94fe285dea0c5dba473ec75b1ebd1fe
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-13 12:11:11 +0200

    [TASK] - datatype:selectSingle - Backend

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,6 +6,7 @@ CREATE TABLE tx_gjoexdatatypes_domain_model_datatype (
   string_with_break text NOT NULL,
   password varchar(20) NOT NULL DEFAULT '',
   number_integer int(11) NOT NULL DEFAULT '0',
+  select_single int(11) NOT NULL DEFAULT '0',
 
   PRIMARY KEY (uid),
   KEY parent (pid),
