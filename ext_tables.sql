e7e4c41e03af7335251ea362917aaf941ec4ef5f
Gregory Jo <gregory.jo@gjo-se.com>
2014-06-13 09:18:22 +0200

    [TASK] - datatype:numberInteger - Backend

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE tx_gjoexdatatypes_domain_model_datatype (
   string_without_break varchar(30) NOT NULL DEFAULT '',
   string_with_break text NOT NULL,
   password varchar(20) NOT NULL DEFAULT '',
+  number_integer int(11) NOT NULL DEFAULT '0',
 
   PRIMARY KEY (uid),
   KEY parent (pid),
